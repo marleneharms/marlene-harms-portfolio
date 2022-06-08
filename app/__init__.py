9e315271280bc75e5af6a77b31a9b480f24dc725
Mateo Martinez <mateo.martinez.spc@icloud.com>
2022-06-07 23:25:23 -0500

    added template layouts

--- app/__init__.py
+++ app/__init__.py
@@ -5,35 +5,40 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+indexData = {
+    "img": "img/mateo.jpeg",
+    "name": "Mateo Marinez",
+    "intro": "Computer Science Student @ The University of Texas Rio Grande Valley"
+}
 
 placesData = {
-    "marlene": "https://www.google.com/maps/d/u/0/embed?mid=1kwRac9a4QDa_pBgkNxwCidA5ocNJhyk&ehbc=2E312F",
-    "mateo": "https://www.google.com/maps/d/embed?mid=1TpVtIh2KYkDkKg-R2-0ZEr8ml0NC8jk&ehbc=2E312F",
-    "nacho": "https://www.google.com/maps/d/u/0/embed?mid=1wnVKNWLtvy_lz9heOEUvS6CPmEUl79E&usp=sharing"
+    "map": "https://www.google.com/maps/d/embed?mid=1TpVtIh2KYkDkKg-R2-0ZEr8ml0NC8jk&ehbc=2E312F",
 }
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', indexData=indexData)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html',)
+
 @app.route('/places')
 def places():
-    return render_template('places.html', maps=placesData)
+    return render_template('places.html', placesData=placesData)
 
 @app.route('/experience')
 def experience():
     return render_template('experience.html')
 
-@app.route('/hobbies')
-def hobbies():
-    nacho_hobbies = [{'desc': 'Nacho hobby 1'}, {'desc': 'Nacho hobby 2'}]
-    mateo_hobbies = [{'desc': 'Mateo hobby 1'}, {'desc': 'Mateo hobby 2'}]
-    marlene_hobbies = [{'desc': 'Marlene hobby 1'}, {'desc': 'Marlene hobby 2'}]
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"), nacho_hobbies=nacho_hobbies, mateo_hobbies=mateo_hobbies, marlene_hobbies=marlene_hobbies) 
+ 
 
