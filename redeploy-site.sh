c59b4d1e6ce8f38880e6c4e4e31276c9713fdfdc
Marlene Harms <marlene_harms@hotmail.com>
2022-06-20 00:15:26 -0500

    redeploy script

---
+++ redeploy-site.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+tmux kill-server
+cd marlene-harms-portfolio
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+tmux new-session -d -s ENTER
+tmux detach -s ENTER
+tmux send-keys -t 0 "flask run --host="0.0.0.0"" ENTER
+echo "Everything is working:D"
