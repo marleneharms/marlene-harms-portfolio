839c974ddb23a63c68f8b39169cf58788b1d995c
Marlene Harms <marlene_harms@hotmail.com>
2022-06-27 03:02:42 -0500

    hotfix

--- app/__init__.py
+++ app/__init__.py
@@ -37,16 +37,16 @@ placesData = {
 }
 
 
-@app.route('/api/timeline', methods=['POST'])
+@app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
     name = request.form['name']
     email = request.form['email']
     content = request.form['content']
-    timeline_post = TimelinePost(name=name, email=email, content=content)
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
     
     return model_to_dict(timeline_post)
 
-@app.route('/api/timeline', methods=['GET'])
+@app.route('/api/timeline_post', methods=['GET'])
 def get_time_line_posts():
     return {
         'time_line_posts': [
