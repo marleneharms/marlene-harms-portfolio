cea7dafe4ecc849d6c11208cf084ba76c729921c
Marlene Harms <marlene_harms@hotmail.com>
2022-06-27 02:52:25 -0500

    added script to test curl

---
+++ curl-test.sh
@@ -0,0 +1,3 @@
+curl -X POST http://marlene-harms.duckdns.org:5000/api/timeline_post -d 'name=marlene&email=marlene_harms@hotmail.com%content=hello world'
+
+curl http://marlene-harms.duckdns.org:5000/api/timeline_post
